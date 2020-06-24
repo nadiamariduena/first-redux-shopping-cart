b1299af840cdd6a70332add77720d3be4ae4cea1
melissa <nadia.mm.mariduena@gmail.com>
2020-06-24 21:12:46 +0200

    Updates

---
+++ precache-manifest.c2f5c8e9a94823cdc4e37b9e1e91b411.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d465223ecb45abf1216416821248fb",
+    "url": "/first-redux-shopping-cart/index.html"
+  },
+  {
+    "revision": "15396ab73dee86de07bd",
+    "url": "/first-redux-shopping-cart/static/css/main.c0387b68.chunk.css"
+  },
+  {
+    "revision": "d16f4ab3859484e70ada",
+    "url": "/first-redux-shopping-cart/static/js/2.9d3f770c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/first-redux-shopping-cart/static/js/2.9d3f770c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15396ab73dee86de07bd",
+    "url": "/first-redux-shopping-cart/static/js/main.7d3d5658.chunk.js"
+  },
+  {
+    "revision": "06c2c1a2e5d617335fec",
+    "url": "/first-redux-shopping-cart/static/js/runtime-main.0b4fa6fd.js"
+  },
+  {
+    "revision": "3c77efe97842c126eeb1c933a366e66b",
+    "url": "/first-redux-shopping-cart/static/media/TylerMitchell-metalmagazine-2.3c77efe9.jpg"
+  }
+]);
